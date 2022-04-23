43614ef9639c0ab2b8540b6146600fa6c19b3154
nathanielruble <rublenathaniel@gmail.com>
2022-04-23 04:34:39 +0100

    First commit from the raspi a+, got i2c probably working on this device, haven't tested it with the stm

---
+++ .bash_history
@@ -0,0 +1,16 @@
+sudo apt-get smbus2
+sudo apt-get install smbus2
+pip3 install smbus2
+sudo apt-get install pip3
+sudo apt-get install python3-pip
+pip3 install smbus2
+ls
+sudo apt update
+sudo apt upgrade
+sudo apt-get install git
+vim i2cmastertest.py
+sudo apt-get install vim
+vim i2cmastertest.py
+sudo raspi-config
+clear
+sudo raspi-config
--- i2cmastertest.py
+++ i2cmastertest.py
@@ -1,19 +1,18 @@
 from smbus2 import SMBus, i2c_msg
-import time
 
+slaveAddress = 0x9
 with SMBus(1) as bus:
     while 1:
-    # Write a single byte to address 0x69
         toSend = input("Enter Message: ")
-        msg = i2c_msg.write(0x69, toSend)
+        msg = i2c_msg.write(slaveAddress, toSend)
         bus.i2c_rdwr(msg)
- 
-        msg = i2c_msg.read(0x69, 18 + len(toSend))
+
+        msg = i2c_msg.read(slaveAddress, 18 + len(toSend))
         bus.i2c_rdwr(msg)
         data = list(msg)
         res = ""
         for i in data:
             res += chr(i)
-        res += '\n'
+        res += "\n"
 
         print(res)
